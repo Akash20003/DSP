e050a7787ea2288e8fea7c8e03811b07eaac5eed
Akash Kulkarni <83762261+Akash20003@users.noreply.github.com>
2021-07-11 20:03:42 +0530

    Code Updated

--- DFT_stem.py
+++ DFT_stem.py
@@ -28,11 +28,21 @@ for j in XK:
     XKr.append(j.real)
     XKi.append(j.imag)
 
+plt.subplot(2,1,1)
+plt.plot(range(0,N),xn)
+plt.title('Input-xn(Time Domain)')
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.grid()
+plt.tight_layout()
+
+plt.subplot(2,1,2)
 plt.stem(XKr,XKi)
-plt.title('DFT(Time to Frequency Domain)')
+plt.title('Output-XK DFT(Time to Frequency Domain)')
 plt.xlabel('Frequency\n(Real)')
 plt.ylabel('Amplitude\n(Imag)')
 plt.grid()
+plt.tight_layout()
 plt.show()
 
 print("\nProgrammed By Akash Kulkarni")
